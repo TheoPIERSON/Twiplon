20af867ccc1283202aeaad077a748b7ead673f6d
Theo Pierson <thheopierson@gmail.com>
2023-09-07 14:03:10 +0200

    feat modify like text

--- src/main/resources/static/js/posts.js
+++ src/main/resources/static/js/posts.js
@@ -7,17 +7,24 @@ btnLikes.forEach((btnLike) => {
     let likedIcon = btnLike.querySelector(".likedIcon");
     let unLikedIcon = btnLike.querySelector(".unlikedIcon");
     let isLiked = btnLike.getAttribute("data-liked") === "true"; // Obtenez l'état actuel du bouton
+    let likeStatus = btnLike.querySelector(".likeStatus");
 
     if (isLiked) {
       btnLike.setAttribute("data-liked", "false"); // Inversez l'état
       likedIcon.style.display = "none";
       unLikedIcon.style.display = "flex";
       console.log("Disliked");
+
+      // Mettez à jour la variable de modèle Thymeleaf
+      likeStatus.textContent = "Soyez le premier à aimer ce post";
     } else {
       btnLike.setAttribute("data-liked", "true"); // Inversez l'état
       likedIcon.style.display = "flex";
       unLikedIcon.style.display = "none";
       console.log("Liked");
+
+      // Mettez à jour la variable de modèle Thymeleaf
+      likeStatus.textContent = "Vous aimez ce post";
     }
   });
 });
